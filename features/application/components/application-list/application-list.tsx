a3c317d9637829dc8838269709dfcc74a7bd2505
Bilal <animeotaku304@gmail.com>
2026-06-26 09:03:17 +0100

    refactor: reduce application list grid columns from four to three for better layout balance

--- features/application/components/application-list/application-list.tsx
+++ features/application/components/application-list/application-list.tsx
@@ -37,7 +37,7 @@ export default async function ApplicationList() {
           </span>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-6">
           {applications.map((app) => (
             <Link
               key={app.id}
