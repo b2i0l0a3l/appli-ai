1f844e6a62eabfa27edc6a03f885501d3fc7aefe
Bilal <animeotaku304@gmail.com>
2026-06-29 14:42:23 +0100

    refactor: remove redundant global CSS and unused layout imports/providers

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,28 +1,11 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/layout/app-sidebar";
-import { cn } from "@/lib/utils";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import Navbar from "@/components/layout/navbar/Navbar";
 import SideBarHeader from "@/components/layout/side-bar-header";
 import { Toaster } from "sonner";
 import Footer from "@/components/layout/footer";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Appli AI",
